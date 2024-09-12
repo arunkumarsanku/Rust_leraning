c7d74b9337061afbe26662d7a72c1796aeb5ef35
Arunkumar Sanku <arunkumarsanku0408@gmail.com>
2024-09-12 22:28:07 +0200

    nums

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
+
+// Define the main function
 fn main() {
+    // Call the is_even function with the argument 21 and store the result in the variable ans
     let ans = is_even(21);
+    // Print the value of ans
     println!("{}", ans);
 }
 
+// Define the is_even function that takes an i32 argument num and returns a bool
 fn is_even(num: i32) -> bool {
-   if num % 2 == 0{
-         return true;
+    // Check if num is divisible by 2 with no remainder
+    if num % 2 == 0 {
+        // If it is, return true
+        return true;
     } else {
-         return false;
-   }
+        // If it's not, return false
+        return false;
+    }
 }
+ 
