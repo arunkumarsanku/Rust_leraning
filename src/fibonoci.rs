9616a3ec922215e09fe8258cd5cb3add0931e7c0
Arunkumar Sanku <arunkumarsanku0408@gmail.com>
2024-09-12 22:54:55 +0200

    fibonoci

--- src/fibonoci.rs
+++ src/fibonoci.rs
@@ -1,25 +1,24 @@
-// write a function fib that finds the fibonacci number it takes as input
-// default is const
 
+/// Calculates the Fibonacci number for the given input.
 fn main() {
     println!("{}", fib(10));
 }
 
 fn fib(num: u32) -> u32 {
-    let mut first = 0;
-    let mut second = 1;
+    let mut first = 0; // Initialize the first Fibonacci number
+    let mut second = 1; // Initialize the second Fibonacci number
 
     if num == 0 {
-        return first;
+        return first; // Return the first Fibonacci number if input is 0
     }
     if num == 1 {
-        return second;
+        return second; // Return the second Fibonacci number if input is 1
     }
 
     for _ in 2..num {
-        let temp = second;
-        second = first + second;
-        first = temp;
+        let temp = second; // Store the current second Fibonacci number
+        second = first + second; // Calculate the next Fibonacci number
+        first = temp; // Update the first Fibonacci number
     }
-    return second;
+    return second; // Return the calculated Fibonacci number
 }
